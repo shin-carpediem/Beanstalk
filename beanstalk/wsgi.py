74604436b5297395171d3e103b151baa004767af
Debian <bitnami@ip-172-26-3-196.ap-northeast-1.compute.internal>
2021-05-29 11:12:22 +0000

    from lightsail

---
+++ beanstalk/wsgi.py
@@ -0,0 +1,19 @@
+"""
+WSGI config for beanstalk project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
+"""
+
+import os
+import sys
+
+sys.path.append('/home/bitnami/apps/django/django_projects/beanstalk')
+
+os.environ.setdefault("PYTHON_EGG_CACHE", "/home/bitnami/apps/django/django_projects/beanstalk/egg_cache")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'beanstalk.settings')
+
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
