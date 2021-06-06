0c780db2ffde5c359288906a9e8981fb9a89f944
shin-carpediem <buru.aoshin@gmail.com>
2021-06-06 16:51:55 +0900

    manage

--- restaurant/views.py
+++ restaurant/views.py
@@ -3,6 +3,7 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.http import require_POST
 from account.models import User
+from .models import Allergy, Category, Menu
 
 
 # Create your views here.
@@ -30,4 +31,16 @@ def manage_login(request):
 
 @login_required
 def manage_menu(request):
-    return render(request, 'restaurant/manage/menu.html')
+    user = request.user
+    restaurant_name = user.name
+
+    categories = Category.objects.all().order_by('id')
+    first_category = Category(id=2)
+    menus = Menu.objects.filter(category=first_category).order_by('-id')
+
+    ctx = {
+        'restaurant_name': restaurant_name,
+        'categories': categories,
+        'menus': menus,
+    }
+    return render(request, 'customer/menu.html', ctx)
