ca38e2bcfac777befdb137949b963a83fb6b808a
shin-carpediem <buru.aoshin@gmail.com>
2021-07-17 23:34:08 +0900

    add test file

---
+++ tests/account_model.py
@@ -0,0 +1,12 @@
+# import pytest
+# from account.models import User, nonLoginUser
+
+
+# @pytest.mark.django_db
+# def test_user():
+#     user = User(email="test")
+
+
+# @pytest.mark.django_db
+# def test_non_login_user():
+#     non_login_user = nonLoginUser(email="test")
